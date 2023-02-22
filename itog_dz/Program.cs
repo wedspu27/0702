2739236999ba0437e3de20bba5b1d723465b1625
mary <wedspu27@gmail.com>
2023-02-22 10:05:04 +0300

    add new task

--- itog_dz/Program.cs
+++ itog_dz/Program.cs
@@ -1,64 +1,76 @@
-﻿// Задача
-// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-
-// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
-// либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
-
-
-// функции:
-// функция вывода массива
-void PrintArray (int[, ]array)
+﻿string? yesNo = "";
+while (yesNo.ToLower() != "yes" && yesNo.ToLower() != "no")
 {
-    for (int i=0; i<array.GetLength(0); i++) // условие для строк
-    {
-        for (int j=0; j<array.GetLength(1); j++) // условие для столбцов
-        {
-            Console.Write($"{array[i, j]} "); // отображение в консоль
-        }
-        Console.WriteLine(); // отступ
-    }
-}
-// функция заполнения массива
-void FillArray (int[, ]array)
-{
-    for (int i=0; i<array.GetLength(0); i++)
-    {
-        for (int j=0; j<array.GetLength(1); j++)
-        {
-            array[i,j] = new Random().Next(-999,1000);  // рандомное число от -999 до 999 
-        }
-    }
-}
-// функция переноса данных из одного массива в другой
-void Perenos1to2 (int[,] array1, int[,] array2)  
-{
-    for (int i=0; i<array1.GetLength(0); i++)
-    {
-        for (int j=0; j<array1.GetLength(1); j++)
-        {
-            array2[i, j] = array1[i, j];
-        }
-    }
+  Console.Write("Желаете вводить строки вручную? (yes/no): ");
+  yesNo = Console.ReadLine();
 }
 
+string[] arrayOfStrings = new string[]  {};
 
-//создаем несколько массивов , во второй будем записывать результат от 1го
-int [, ] array1 = new int [2, 2];
-int [, ] array2 = new int [2, 2];
+if (yesNo.ToLower() == "yes") {
+  int n = InputNumbers("Введите количество элементов массива: ");
+  arrayOfStrings = new string[n];
+  for (int i = 0; i < arrayOfStrings.Length; i++)
+  {
+    Console.Write($" Введите {i+1} строку: ");
+    arrayOfStrings[i] = Console.ReadLine();
+  }
+}
+else 
+{
+  arrayOfStrings = new string[]  {"hello", "2", "world", ":-)"};
+}
 
-// применяем функции по заполнению и выводу
-FillArray(array1);
-Console.WriteLine();
-PrintArray(array1);
-Console.WriteLine();
+int lengthLimit = 3;
 
-FillArray(array2);
-Console.WriteLine();
-PrintArray(array2);
-Console.WriteLine();
+int numbersItems = CheckArray(arrayOfStrings, lengthLimit);
+
+string[] newArrayOfStrings = new string[numbersItems];
+
+FillNewArray(arrayOfStrings, newArrayOfStrings, lengthLimit);
+
+Console.WriteLine($"{PrintArray(newArrayOfStrings)}");
 
 
-// вывод итогового массива и комментария
-Perenos1to2(array1, array2);
-Console.WriteLine($"\aarray2 is:");
-PrintArray(array2);
+void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+  int temp = 0;
+  for (int i = 0; i < oldArray.Length; i++)
+  {
+    if (oldArray[i].Length <= lengthLimit)
+    {
+      newArray[temp] = oldArray[i];
+      temp++;
+    }
+  }
+}
+
+int CheckArray(string[] array, int lengthLimit)
+{
+  int result = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= lengthLimit) result++;
+  }
+  return result;
+}
+
+string PrintArray(string[] array)
+{ 
+  string result = string.Empty;
+  result = "[ ";
+  for (int i = 0; i < array.Length; i++)
+  {
+    result += $"{array[i],1}";
+    if (i < array.Length - 1) result += ", ";
+  }
+  result += " ]";
+  return result;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
